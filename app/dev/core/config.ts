9f52460046072b43b050ce00adaac526f035bc6b
Martin Spierings <martin@martinspire.nl>
2015-10-15 20:23:25 +0200

    lint fixes

--- app/dev/core/config.ts
+++ app/dev/core/config.ts
@@ -1,4 +1,4 @@
 angular.module('ipot').constant('AzureMobileServiceClient', {
     API_URL : 'https://i-pot.azure-mobile.net/',
-    API_KEY : 'lnKgquJOBbjXztszZJfZhQfQagRhBA89',
+    API_KEY : 'lnKgquJOBbjXztszZJfZhQfQagRhBA89'
 });
